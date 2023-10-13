7475a67a5879637a4ad3835ba85e744be90fa1f1
Valdir Torres Borges <valdir@localhost.localdomain>
2023-10-13 14:26:18 -0300

    Aula 2 finalizada

---
+++ README-valdir.md
@@ -0,0 +1,5 @@
+Acessar banco
+localhost:8080/h2-console
+JDBC URL: jdbc:h2:mem:todolist
+admin
+admin
--- src/main/java/br/com/valdirtorres/todolist/user/UserController.java
+++ src/main/java/br/com/valdirtorres/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.valdirtorres.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,19 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /**
-     * String (texto)
-     * Integer (int) numeros inteiros
-     * Double (double) Números 0.0000
-     * Float (float) Números 0.000
-     * char (A C )
-     * Date (data)
-     * void
-     */
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
-        
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("Usuário já existe");
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
     
 }
